edc1ac806a502328e3d017e52e8eee53fe002653
de3key <gnosthard@gmail.com>
2021-10-30 14:48:26 +0700

    hi

---
+++ 29.10/25.10/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace _25._10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 41);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 57);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "                                       Онлайн библиотека";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(22, 102);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(629, 284);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(22, 405);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(121, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(655, 318);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 68);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Max";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(655, 246);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 68);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Min";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(200, 390);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 68);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Записать ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(346, 390);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 68);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Читать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Автор";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Год издания";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Название книги";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Кол-стр";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 390);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Кол-Книг нужных тебе";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 554);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 29.10/25.10/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace _25._10
+{
+
+    public partial class Form1 : Form
+    {
+        public struct Person 
+        {
+            public string aftor;
+            public DateTime date;
+            public string name;
+            public int kolstr;
+  
+        }
+    public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = dataGridView1.RowCount = Convert.ToInt32(numericUpDown1.Value);
+            Person[] mas = new Person[n];            
+            for (int i = 0; i < n; i++)
+            {
+                mas[i].aftor = dataGridView1[0, i].Value.ToString();
+                mas[i].date = Convert.ToDateTime(dataGridView1[1, i].Value);
+                mas[i].name = dataGridView1[2, i].Value.ToString();
+                mas[i].kolstr = Convert.ToInt32(dataGridView1[3, i].Value);
+            }
+            int max = mas[0].kolstr;
+            for (int i = 0; i < n; i++)
+            {
+                if (max < mas[i].kolstr)
+                {
+                    max = mas[i].kolstr;
+                }
+            }
+            MessageBox.Show(max.ToString());
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n = dataGridView1.RowCount = Convert.ToInt32(numericUpDown1.Value);
+            Person[] mas = new Person[n];
+            for (int i = 0; i < n; i++)
+            {
+                mas[i].aftor = dataGridView1[0, i].Value.ToString();
+                mas[i].date = Convert.ToDateTime(dataGridView1[1, i].Value);
+                mas[i].name = dataGridView1[2, i].Value.ToString();
+                mas[i].kolstr = Convert.ToInt32(dataGridView1[3, i].Value);
+            }
+            int min = mas[0].kolstr; 
+            for (int i = 0; i < n; i++)
+            {
+                if (min > mas[i].kolstr)
+                {
+
+                  min = mas[i].kolstr;
+                }
+            }
+            MessageBox.Show(min.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                int n = dataGridView1.RowCount = Convert.ToInt32(numericUpDown1.Value);
+                Person[] mas = new Person[n];
+                for (int i = 0; i < n; i++)
+                {
+                    mas[i].aftor = dataGridView1[0, i].Value.ToString();
+                    mas[i].date = Convert.ToDateTime(dataGridView1[1, i].Value);
+                    mas[i].name = dataGridView1[2, i].Value.ToString();
+                    mas[i].kolstr = Convert.ToInt32(dataGridView1[3, i].Value);
+                }
+                StreamWriter sw = new StreamWriter(saveFileDialog1.FileName, true, System.Text.Encoding.Default);
+                for (int i = 0; i < n; i++) 
+                {
+                    sw.WriteLine(mas[i].aftor);
+                    sw.WriteLine(mas[i].date);
+                    sw.WriteLine(mas[i].name);
+                    sw.WriteLine(mas[i].kolstr);
+                }
+                sw.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) 
+            {
+                StreamReader sr = new StreamReader(saveFileDialog1.FileName,System.Text.Encoding.Default);
+                Person[] mas = new Person[0];
+                int k = 0;
+                while (!sr.EndOfStream)
+                {
+                    k++;
+                    Array.Resize(ref mas, k);
+                    mas[k - 1].aftor = sr.ReadLine();
+                    mas[k - 1].date =Convert.ToDateTime(sr.ReadLine());
+                    mas[k - 1].name = sr.ReadLine();
+                    mas[k - 1].kolstr = Convert.ToInt32(sr.ReadLine());
+                }
+                sr.Close();
+                dataGridView1.RowCount = k;
+                for (int i = 0; i < k; i++) 
+                {
+                    dataGridView1[0, i].Value = mas[i].aftor;
+                    dataGridView1[1, i].Value = mas[i].date;
+                    dataGridView1[2, i].Value = mas[i].name;
+                    dataGridView1[3, i].Value = mas[i].kolstr;
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
